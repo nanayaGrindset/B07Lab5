ab5c2ac5df4fdada12d4424b1bace3b94411de9e
Zhiyanliu531 <iszhiyanliu@gmail.com>
2025-10-21 16:31:18 -0400

    feat: add unsaved codes in circle class

---
+++ Circle.java
@@ -0,0 +1,27 @@
+import Point.java;
+
+public class Circle {
+    point center;
+    double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
